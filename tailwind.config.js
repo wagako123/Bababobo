416d99aff4b432c027fe71d04cce75c3c6fb4b01
unknown <brianwagako@gmail.com>
2023-08-11 16:19:18 +0300

    stuckcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
     extend: {
       colors:{
         "color-primary":"#01051e",
-        "color-primary-light":"#020726",
+        "color-primary-light":"#50b8e7",
         "color-primary-dark":"#010417",
         "color-secondary":"#ff7d3d",
         "color-gray":"#333",
